d6fde6b943e3e0041fea0e3f8de24dbc5377e0a5
nick adams <thebigdeal916@icloud.com>
2020-05-21 18:25:59 -0700

    Updates

---
+++ precache-manifest.78c72887397e747a59f97b80d8bf6e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5958be4f395edc94dd5a",
+    "url": "/Project-3/static/css/main.797cb696.chunk.css"
+  },
+  {
+    "revision": "5958be4f395edc94dd5a",
+    "url": "/Project-3/static/js/main.d07ad46a.chunk.js"
+  },
+  {
+    "revision": "e0f3bcecaaff3d552af1",
+    "url": "/Project-3/static/js/runtime~main.ef102b0a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/Project-3/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/Project-3/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "ca44baf0afebf61907327aaed38522b5",
+    "url": "/Project-3/index.html"
+  }
+];
